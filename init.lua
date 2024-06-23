3d350976d8778fecab6fca559cafef914ccf537a
thisjustinf <jbfagan7@gmail.com>
2024-06-23 14:33:27 -0400

    fix: removed transparency code

--- init.lua
+++ init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.api.nvim_create_autocmd("ColorScheme", {
-  pattern = "*",
-  callback = function()
-    require("config.theme").setup()
-  end,
-})
