dd8b41dd1075f6964a42f0211a404e91444f469d
thisjustinf <40900036+thisjustinf@users.noreply.github.com>
2026-02-27 17:47:36 -0500

    feat: added evangelion theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,27 @@ return {
   { "ellisonleao/gruvbox.nvim" },
   { "shaunsingh/moonlight.nvim" },
   {
-    "samharju/synthweave.nvim",
+    "oxfist/night-owl.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      require("night-owl").setup({
+        transparent_background = true,
+      })
+      vim.cmd.colorscheme("night-owl")
+    end,
+  },
+  {
+    "xero/evangelion.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("evangelion").setup({
+        transparent = true,
+      })
+      vim.cmd.colorscheme("evangelion")
+    end,
   },
   {
     "scottmckendry/cyberdream.nvim",
